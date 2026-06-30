a223ee308a4cd9ae8eb4db344c615a523a5532d2
danilaplee <fiend.of.sound@gmail.com>
2026-07-01 01:16:48 +0200

    feat/add-prebuilt

---
+++ ffmpeg-kit/prebuilt/apple-ios-x86_64-mac-catalyst/libilbc/include/ilbc_export.h
@@ -0,0 +1,43 @@
+
+#ifndef ILBC_EXPORT_H
+#define ILBC_EXPORT_H
+
+#ifdef ILBC_STATIC_DEFINE
+#  define ILBC_EXPORT
+#  define ILBC_NO_EXPORT
+#else
+#  ifndef ILBC_EXPORT
+#    ifdef ilbc_EXPORTS
+        /* We are building this library */
+#      define ILBC_EXPORT 
+#    else
+        /* We are using this library */
+#      define ILBC_EXPORT 
+#    endif
+#  endif
+
+#  ifndef ILBC_NO_EXPORT
+#    define ILBC_NO_EXPORT 
+#  endif
+#endif
+
+#ifndef ILBC_DEPRECATED
+#  define ILBC_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef ILBC_DEPRECATED_EXPORT
+#  define ILBC_DEPRECATED_EXPORT ILBC_EXPORT ILBC_DEPRECATED
+#endif
+
+#ifndef ILBC_DEPRECATED_NO_EXPORT
+#  define ILBC_DEPRECATED_NO_EXPORT ILBC_NO_EXPORT ILBC_DEPRECATED
+#endif
+
+/* NOLINTNEXTLINE(readability-avoid-unconditional-preprocessor-if) */
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef ILBC_NO_DEPRECATED
+#    define ILBC_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* ILBC_EXPORT_H */
